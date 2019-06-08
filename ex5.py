bfd9ea0bc943a0ea5600fe4994b1b47b752ea56c
Nick Kuizenga <nickkuis@hotmail.com>
2019-06-08 13:23:11 -0400

    Finished ex5

--- ex5.py
+++ ex5.py
@@ -13,7 +13,7 @@ if args.concat:
     f = open(args.concat,"a+")
     for file in args.files:
         open_file = open(file, "r")
-        read_convert = str(o.read())
+        read_convert = str(open_file.read())
         if open_file.mode == "r":
             f.write(read_convert)
     f.close() 
@@ -24,6 +24,19 @@ else:
         if f.mode == 'r':
             print f.read()
 
+if args.concat and args.n == True:
+    with open(args.concat, "rw") as f:
+        line = f.readline()
+        count = 1
+        while line:
+            # print count, line
+            line = line.strip()
+            new_line = count, line
+            f.write(new_line)
+            count += 1
+
+
 
 # 20 minutes read excercise & implemented basic cat functionality
 # Implementing additional cat flags (from man cat manual)
+# 25 minutes trying to implement line numbering
